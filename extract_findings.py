7f39bb0f265f5b0167fca6af9549c3a145acb9a0
Ricardo Pereira - Veracode <20283870+cadonuno@users.noreply.github.com>
2025-03-07 18:44:16 +0000

    Adding OWASP info and check for new findings

--- extract_findings.py
+++ extract_findings.py
@@ -26,6 +26,8 @@ severity_map = {
     5: "Very High",
 }
 
+owasp_cache = {}
+
 def print_help():
     print("""extractfindings.py -t <target_file(.xlsx)> -a <application_guid> [-s] [-d]
         Gets ALL findings for a specific application profile, including latest scan name and date, and saves it to <target_file>""")
@@ -117,11 +119,18 @@ def get_exploitability(cve_node):
             "percentile": cve_node['exploitability']['epss_percentile']
         }
 
-def try_decode(element):
+def is_encoded(element):
     try:
-        return base64.b64decode(element)
+        return base64.b64encode(base64.b64decode(element)) == element
     except Exception:
-        return element
+        return False
+
+def try_decode(element):
+    if is_encoded(element):    
+        try:
+            return base64.b64decode(element)
+        except Exception:
+            return element
 
 def write_row(worksheet, row, content):
     column = 1
@@ -129,43 +138,89 @@ def write_row(worksheet, row, content):
         worksheet.cell(row=row, column=column).value = value
         column+=1
 
-def save_sast(sast_findings, worksheet):
-    write_row(worksheet, 1, ["Issue ID", "Description", "Violates Policy?", "First Found Date", "Status", "Resolution", "Severity", "CWE", "Finding Category", "Module Name", "File Path", "Line"])
+def call_owasp_api(cwe_node, cwe_id, verbose):
+    print(f"Getting OWASP info for CWE-{cwe_id}")
+    path = cwe_node["href"]
+
+    if verbose:
+        print(f"Calling API at {path}")
+
+    response = requests.get(path, auth=RequestsAuthPluginVeracodeHMAC(), headers=json_header)
+
+    body = response.json()
+    if verbose:
+        print(f"status code {response.status_code}")
+        if body:
+            print(body)
+    if response.status_code == 200:
+        if "references":
+            references = body["references"]
+            for reference in references:
+                if reference["name"] == "OWASP":
+                    return True
+        return False
+    elif response.status_code == 429:
+        handle_throttling()
+        return call_owasp_api(cwe_node, cwe_id, verbose)
+    else:
+        print(f"Unable to obtain OWASP info for CWE-{cwe_id}: {response.status_code}")
+        return None
+
+def parse_owasp_info(finding_details, verbose):
+    global owasp_cache
+    if not "cwe" in finding_details or not "id" in finding_details["cwe"]:
+        return False
+    cwe_id = finding_details["cwe"]["id"]
+    if cwe_id in owasp_cache:
+        return owasp_cache[cwe_id]
+
+    owasp_info = call_owasp_api(finding_details["cwe"], cwe_id, verbose)
+
+    if owasp_info == None:
+        return False
+    
+    owasp_cache[cwe_id] = owasp_info
+    return owasp_info
+
+def save_sast(sast_findings, worksheet, verbose):
+    write_row(worksheet, 1, ["Issue ID", "Description", "Violates Policy?", "First Found Date", "Status", "Resolution", "Severity", "CWE", "Finding Category", "Module Name", "File Path", "Line", "Is New?", "Is OWASP?"])
     row = 2
     for finding in sast_findings:
         write_row(worksheet, row, [
                 finding['issue_id'], try_decode(finding['description']), finding['violates_policy'], finding['finding_status']['first_found_date'], finding['finding_status']['status'], 
                 finding['finding_status']['resolution'], severity_map[finding['finding_details']['severity']], 
                 "CWE " + str(finding['finding_details']['cwe']['id']) + ": " + finding['finding_details']['cwe']['name'], finding['finding_details']['finding_category']['name'], 
-                finding['finding_details']['module'], finding['finding_details']['file_path'], finding['finding_details']['file_line_number'] 
+                finding['finding_details']['module'], finding['finding_details']['file_path'], finding['finding_details']['file_line_number'],
+                "Yes" if finding['finding_status']["new"] else "No", "Yes" if parse_owasp_info(finding['finding_details'], verbose) else "No"
             ])
         row+=1
 
-def save_dast(dast_findings, worksheet):
-    write_row(worksheet, 1, ["Issue ID", "Description", "Violates Policy?", "First Found Date", "Status", "Resolution", "Severity", "CWE", "Finding Category", "URL", "Attack Vector", "Vulnerable Parameter"])
+def save_dast(dast_findings, worksheet, verbose):
+    write_row(worksheet, 1, ["Issue ID", "Description", "Violates Policy?", "First Found Date", "Status", "Resolution", "Severity", "CWE", "Finding Category", "URL", "Attack Vector", "Vulnerable Parameter", "Is New?", "Is OWASP?"])
     row = 2
     for finding in dast_findings:
         write_row(worksheet, row, [
                 finding['issue_id'], try_decode(finding['description']), finding['violates_policy'], finding['finding_status']['first_found_date'], finding['finding_status']['status'], 
                 finding['finding_status']['resolution'], severity_map[finding['finding_details']['severity']], 
                 "CWE " + str(finding['finding_details']['cwe']['id']) + ": " + finding['finding_details']['cwe']['name'], finding['finding_details']['finding_category']['name'], 
-                finding['finding_details']['url'], finding['finding_details']['attack_vector'], finding['finding_details']['vulnerable_parameter'] if 'vulnerable_parameter' in finding['finding_details'] else ''
-            ])
+                finding['finding_details']['url'], finding['finding_details']['attack_vector'], finding['finding_details']['vulnerable_parameter'] if 'vulnerable_parameter' in finding['finding_details'] else '',
+                "Yes" if finding['finding_status']["new"] else "No", "Yes" if parse_owasp_info(finding['finding_details'], verbose) else "No"])
         row+=1
 
-def save_sca(sca_findings, worksheet):
-    write_row(worksheet, 1, ["Component File", "Component Version", "Description", "Violates Policy?", "First Found Date", "Status", "Resolution", "Severity", "EPSS Score", "EPSS Percentile", "CVE", "CVSS2", "CVSS3"])
+def save_sca(sca_findings, worksheet, verbose):
+    write_row(worksheet, 1, ["Component File", "Component Version", "Description", "Violates Policy?", "First Found Date", "Status", "Resolution", "Severity", "EPSS Score", "EPSS Percentile", "CVE", "CVSS2", "CVSS3", "Is New", "Is OWASP?"])
     row = 2
     for finding in sca_findings:
         exploitability = get_exploitability(finding['finding_details']['cve'])
+
         write_row(worksheet, row, [finding['finding_details']['component_filename'], finding['finding_details']['version'],
             try_decode(finding['description']), finding['violates_policy'], finding['finding_status']['first_found_date'], finding['finding_status']['status'], 
             finding['finding_status']['resolution'], severity_map[finding['finding_details']['severity']], exploitability["score"], exploitability["percentile"],
             finding['finding_details']['cve']['name'], finding['finding_details']['cve']['cvss'], 
-            finding['finding_details']['cve']['cvss3']['score']])
+            finding['finding_details']['cve']['cvss3']['score'], "Yes" if finding['finding_status']["new"] else "No", "Yes" if parse_owasp_info(finding['finding_details'], verbose) else "No"])
         row+=1
 
-def save_to_excel(findings, file_name):
+def save_to_excel(findings, file_name, verbose):
     directory = os.path.dirname(file_name)
     if directory and not os.path.exists(directory):
         os.makedirs(directory)
@@ -173,11 +228,11 @@ def save_to_excel(findings, file_name):
             workbook = Workbook()
             workbook.remove(workbook.active)
             if findings['sast']:
-                save_sast(findings['sast'], workbook.create_sheet(title="SAST"))
+                save_sast(findings['sast'], workbook.create_sheet(title="SAST"), verbose)
             if findings['dast']:
-                save_dast(findings['dast'], workbook.create_sheet(title="DAST"))
+                save_dast(findings['dast'], workbook.create_sheet(title="DAST"), verbose)
             if findings['sca']:
-                save_sca(findings['sca'], workbook.create_sheet(title="SCA"))
+                save_sca(findings['sca'], workbook.create_sheet(title="SCA"), verbose)
 
             workbook.save(file_name)
     else:
@@ -189,7 +244,7 @@ def save_scan_results(rest_api_base, application_guid, target_file, is_sast, is_
         print(f"ERROR: File name '{target_file}' needs to be a XLSX file.")
         sys.exit(-1)
 
-    save_to_excel(get_application_results(application_guid, rest_api_base, is_sast, is_dast, is_sca, verbose), target_file)
+    save_to_excel(get_application_results(application_guid, rest_api_base, is_sast, is_dast, is_sca, verbose), target_file, verbose)
 
 def main():
     try:        
@@ -201,7 +256,7 @@ def main():
         parser.add_argument('-s', '--sast', action='store_true', help='Set to enable fetching of SAST results')
         parser.add_argument('-d', '--dast', action='store_true', help='Set to enable fetching of DAST results')
         parser.add_argument('-c', '--sca', action='store_true', help='Set to enable fetching of SCA results')
-        parser.add_argument('-a', '--application_guid', help='Set to enable fetching of DAST results')
+        parser.add_argument('-a', '--application_guid', help='GUID of application to fetch')
         parser.add_argument('-v', '--verbose', action='store_true', help='Set to enable verbose logging')
 
         args = parser.parse_args()
